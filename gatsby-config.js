fdb692a983ffacac9feefdb93f2e0edd8c4fc539
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-09-16 13:39:20 -0500

    re-customize gatsby-config, enable opensearch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,43 @@
 module.exports = {
   siteMetadata: {
-    title: 'gatsby-starter-code-notes',
-    description: `A starter for gatsby-theme-code-notes`,
-    author: 'Zander',
+    title: "Alex Jackson's Code Notes",
+    description: `A central repo for my notes`,
+    author: "Alex Jackson",
   },
   plugins: [
     {
-      resolve: 'gatsby-theme-code-notes',
+      resolve: "gatsby-theme-code-notes",
       options: {
-        contentPath: 'notes',
-        basePath: '/',
-        showThemeInfo: true,
+        contentPath: "notes",
+        basePath: "/",
+        showThemeInfo: false,
         showDescriptionInSidebar: true,
+        gitRepoContentPath:
+          "https://github.com/beattheprose/notes.ajax.computer/blob/main/notes/",
+        //Opensearch config
+        openSearch: {
+          siteUrl: `https://notes.ajax.computer`,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Zander's Code Notes`,
+        short_name: `CodeNotes`,
+        description: `Notes on code. My memory bank.`,
+        start_url: `/`,
+        background_color: `hsl(210, 38%, 95%)`,
+        theme_color: `hsl(345, 100%, 69%)`,
+        display: `standalone`,
+        icon: `static/logo.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/*`, `/tag/*`],
       },
     },
   ],
-}
+};
